ceeeafa749023e2b8d25256743e1ea5d315d7ef1
William Hleucka <william.hleucka@gmail.com>
2023-06-29 10:55:19 -0600

    tweak: disable soprano, update flash

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,5 +1,5 @@
 --- Check if a file or directory exists in this path
-function exists(file)
+local function exists(file)
   local ok, err, code = os.rename(file, file)
   if not ok then
     if code == 13 then
@@ -28,22 +28,34 @@ return {
       {
         "s",
         mode = { "n", "x", "o" },
-        function()
-          -- default options: exact mode, multi window, all directions, with a backdrop
-          require("flash").jump()
-        end,
+        function() require("flash").jump() end,
+        desc = "Flash",
       },
       {
         "S",
-        mode = { "o", "x" },
+        mode = { "n", "o", "x" },
         function() require("flash").treesitter() end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function() require("flash").remote() end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc = "Flash Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function() require("flash").toggle() end,
+        desc = "Toggle Flash Search",
       },
     },
-  },
-
-  {
-    "whleucka/soprano.nvim",
-    event = "VeryLazy",
   },
   {
     "nvim-neotest/neotest",
@@ -68,75 +80,79 @@ return {
       }
     end,
   },
-  {
-    "rebelot/heirline.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      local status = require "astronvim.utils.status"
-      local soprano_component = {
-        provider = " Soprano ",
-        on_click = {
-          callback = function()
-            -- Get the ToggleTerm API
-            require("toggleterm").toggle(0)
-          end,
-          name = "soprano_toggle",
-        },
-      }
-      local soprano_prev = {
-        provider = " ⏮ ",
-        on_click = {
-          callback = function()
-            if vim.fn.executable "socat" ~= 1 then error "mpv is not installed." end
-            os.execute "echo playlist-prev | socat - /tmp/mpvsocket"
-          end,
-          name = "previous_track",
-        },
-      }
-      local soprano_play_pause = {
-        provider = " ▶ ",
-        on_click = {
-          callback = function()
-            if vim.fn.executable "socat" ~= 1 then error "mpv is not installed." end
-            os.execute "echo cycle pause | socat - /tmp/mpvsocket"
-          end,
-          name = "play_pause",
-        },
-      }
-      local soprano_next = {
-        provider = " ⏭ ",
-        on_click = {
-          callback = function()
-            if vim.fn.executable "socat" ~= 1 then error "mpv is not installed." end
-            os.execute "echo playlist-next | socat - /tmp/mpvsocket"
-          end,
-          name = "next_track",
-        },
-      }
-
-      opts.statusline = {
-        -- statusline
-        hl = { fg = "fg", bg = "bg" },
-        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
-        status.component.git_branch(),
-        status.component.file_info { filetype = {}, filename = false, file_modified = false },
-        status.component.git_diff(),
-        status.component.diagnostics(),
-        status.component.fill(),
-        status.component.cmd_info(),
-        status.component.fill(),
-        status.component.lsp(),
-        status.component.treesitter(),
-        status.component.nav(),
-        soprano_component,
-        soprano_prev,
-        soprano_play_pause,
-        soprano_next,
-        -- remove the 2nd mode indicator on the right
-      }
-
-      -- return the final configuration table
-      return opts
-    end,
-  },
+  -- {
+  --   "whleucka/soprano.nvim",
+  --   event = "VeryLazy",
+  -- },
+  -- {
+  --   "rebelot/heirline.nvim",
+  --   event = "VeryLazy",
+  --   opts = function(_, opts)
+  --     local status = require "astronvim.utils.status"
+  --     local soprano_component = {
+  --       provider = " Soprano ",
+  --       on_click = {
+  --         callback = function()
+  --           -- Get the ToggleTerm API
+  --           require("toggleterm").toggle(0)
+  --         end,
+  --         name = "soprano_toggle",
+  --       },
+  --     }
+  --     local soprano_prev = {
+  --       provider = " ⏮ ",
+  --       on_click = {
+  --         callback = function()
+  --           if vim.fn.executable "socat" ~= 1 then error "mpv is not installed." end
+  --           os.execute "echo playlist-prev | socat - /tmp/mpvsocket"
+  --         end,
+  --         name = "previous_track",
+  --       },
+  --     }
+  --     local soprano_play_pause = {
+  --       provider = " ▶ ",
+  --       on_click = {
+  --         callback = function()
+  --           if vim.fn.executable "socat" ~= 1 then error "mpv is not installed." end
+  --           os.execute "echo cycle pause | socat - /tmp/mpvsocket"
+  --         end,
+  --         name = "play_pause",
+  --       },
+  --     }
+  --     local soprano_next = {
+  --       provider = " ⏭ ",
+  --       on_click = {
+  --         callback = function()
+  --           if vim.fn.executable "socat" ~= 1 then error "mpv is not installed." end
+  --           os.execute "echo playlist-next | socat - /tmp/mpvsocket"
+  --         end,
+  --         name = "next_track",
+  --       },
+  --     }
+  --
+  --     opts.statusline = {
+  --       -- statusline
+  --       hl = { fg = "fg", bg = "bg" },
+  --       status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+  --       status.component.git_branch(),
+  --       status.component.file_info { filetype = {}, filename = false, file_modified = false },
+  --       status.component.git_diff(),
+  --       status.component.diagnostics(),
+  --       status.component.fill(),
+  --       status.component.cmd_info(),
+  --       status.component.fill(),
+  --       status.component.lsp(),
+  --       status.component.treesitter(),
+  --       status.component.nav(),
+  --       soprano_component,
+  --       soprano_prev,
+  --       soprano_play_pause,
+  --       soprano_next,
+  --       -- remove the 2nd mode indicator on the right
+  --     }
+  --
+  --     -- return the final configuration table
+  --     return opts
+  --   end,
+  -- },
 }
