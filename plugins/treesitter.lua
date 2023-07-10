b19a5c606712de50bc191ffffc3701b63e9127a5
William Hleucka <william.hleucka@gmail.com>
2023-07-10 09:57:00 -0600

    fix: incr selec

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,5 +2,14 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     -- ensure_installed = { "lua" },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        scope_incremental = "<CR>",
+        node_incremental = "<TAB>",
+        node_decremental = "<S-TAB>",
+      },
+    },
   },
 }
