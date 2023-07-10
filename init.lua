8a5521ee2e612d9673c8f34198fb36b8138bca51
William Hleucka <william.hleucka@gmail.com>
2023-07-10 11:07:35 -0600

    fix: remove test clipboard

--- init.lua
+++ init.lua
@@ -78,6 +78,5 @@ return {
   polish = function()
     vim.keymap.set("n", "j", "gj", { silent = true })
     vim.keymap.set("n", "k", "gk", { silent = true })
-    vim.cmd[[set clipboard+=unnamedplus]]
   end,
 }
