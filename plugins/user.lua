c357048621113805104353478bb6013f7e782585
William Hleucka <william.hleucka@gmail.com>
2023-07-10 11:12:23 -0600

    update: clipboard

--- plugins/user.lua
+++ plugins/user.lua
@@ -24,13 +24,20 @@ return {
   {
     "ojroques/vim-oscyank",
     config = function()
-      -- copy to clip
-      vim.keymap.set('n', '<leader>y', '<Plug>OSCYankOperator')
-      -- current line
-      vim.keymap.set('n', '<leader>yy', '<leader>c_', {remap = true})
-      -- copy current selection
-      vim.keymap.set('v', '<leader>y', '<Plug>OSCYankVisual')
-    end
+      local function copy(lines, _) require("osc52").copy(table.concat(lines, "\n")) end
+
+      local function paste() return { vim.fn.split(vim.fn.getreg "", "\n"), vim.fn.getregtype "" } end
+
+      vim.g.clipboard = {
+        name = "osc52",
+        copy = { ["+"] = copy, ["*"] = copy },
+        paste = { ["+"] = paste, ["*"] = paste },
+      }
+
+      -- Now the '+' register will copy to system clipboard using OSC52
+      vim.keymap.set("n", "<leader>y", '"+y')
+      vim.keymap.set("n", "<leader>yy", '"+yy')
+    end,
   },
   -- {
   --   "whleucka/soprano.nvim",
