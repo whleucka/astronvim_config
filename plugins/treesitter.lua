a91dad0e444ece717d2fe3e1611a47e562c5e4e0
William Hleucka <william.hleucka@gmail.com>
2023-07-10 10:08:35 -0600

    fix: incr selec, update mappings

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,10 +5,9 @@ return {
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = "<CR>",
-        scope_incremental = "<CR>",
-        node_incremental = "<TAB>",
-        node_decremental = "<S-TAB>",
+        init_selection = "<Enter>",
+        node_incremental = "<Enter>",
+        node_decremental = "<BS>",
       },
     },
   },
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,20 +1,7 @@
---- Check if a file or directory exists in this path
-local function exists(file)
-  local ok, err, code = os.rename(file, file)
-  if not ok then
-    if code == 13 then
-      -- Permission denied, but it exists
-      return true
-    end
-  end
-  return ok, err
-end
-
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   { "nvim-treesitter/nvim-treesitter-context" },
-  { "barrett-ruth/telescope-http.nvim" },
   {
     "folke/tokyonight.nvim",
     config = require "user.plugins.config.tokyonight",
@@ -24,38 +11,7 @@ return {
     event = "VeryLazy",
     ---@type Flash.Config
     opts = {},
-    keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function() require("flash").jump() end,
-        desc = "Flash",
-      },
-      {
-        "S",
-        mode = { "n", "o", "x" },
-        function() require("flash").treesitter() end,
-        desc = "Flash Treesitter",
-      },
-      {
-        "r",
-        mode = "o",
-        function() require("flash").remote() end,
-        desc = "Remote Flash",
-      },
-      {
-        "R",
-        mode = { "o", "x" },
-        function() require("flash").treesitter_search() end,
-        desc = "Flash Treesitter Search",
-      },
-      {
-        "<c-s>",
-        mode = { "c" },
-        function() require("flash").toggle() end,
-        desc = "Toggle Flash Search",
-      },
-    },
+    keys = require "user.plugins.config.flash-keys",
   },
   {
     "nvim-neotest/neotest",
@@ -63,25 +19,13 @@ return {
     dependencies = {
       "olimorris/neotest-phpunit",
     },
-    config = function()
-      require("neotest").setup {
-        adapters = {
-          require "neotest-phpunit" {
-            phpunit_cmd = function()
-              if exists "./vendor/bin/phpunit" then
-                -- composer phpuit
-                return "./vendor/bin/phpunit"
-              end
-              -- phar in bin
-              return "./bin/phpunit"
-            end,
-          },
-        },
-      }
-    end,
+    config = function() require "user.plugins.config.neotest" end,
   },
   -- {
   --   "whleucka/soprano.nvim",
+  --   dependencies = {
+  --    "barrett-ruth/telescope-http.nvim",
+  --   }
   --   event = "VeryLazy",
   -- },
   -- {
