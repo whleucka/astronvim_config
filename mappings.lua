66777202842114dba765cf986a928e90ec73e9a9
William Hleucka <william.hleucka@gmail.com>
2023-07-10 08:43:27 -0600

    Update mappings

--- mappings.lua
+++ mappings.lua
@@ -7,9 +7,8 @@ return {
   -- first key is the mode
   n = {
     ["<c-g>"] = { "<cmd>Neogit kind=split<cr>", desc = "Floating terminal" },
-    ["<c-p>"] = { "<cmd>Telescope find_files<cr>", desc = "Floating terminal" },
-    ["<leader>["] = { "^", desc = "BOL" },
-    ["<leader>]"] = { "$", desc = "EOL" },
+    ["<leader>]"] = { "<cmd>cnext<cr>", desc = "Quickfix next" },
+    ["<leader>["] = { "<cmd>cprev<cr>", desc = "Quickfix prev" },
     ["<leader>ss"] = { "<cmd>lua require('soprano').search()<cr>", desc = "Soprano music search" },
     ["<leader>sr"] = { "<cmd>lua require('soprano').radio()<cr>", desc = "Soprano radio" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
@@ -22,7 +21,6 @@ return {
       desc = "Pick to close",
     },
     ["<leader>b"] = { name = "Buffers" },
-    -- Custom mappings below
     H = { "<cmd>bprevious<CR>", desc = "Left buffer" },
     L = { "<cmd>bnext<CR>", desc = "Right buffer" },
     ["<leader>rr"] = { "<cmd>RustRunnables<cr>", desc = "Rust Runnables" },
@@ -36,7 +34,9 @@ return {
     ["<leader>is"] = { "<cmd>lua require('neotest').summary.toggle({ enter = true })<cr>", desc = "PHPUnit toggle test summary"},
     ["<leader>io"] = { "<cmd>lua require('neotest').output_panel.toggle({ enter = true })<cr>", desc = "PHPUnit toggle output panel"},
     ["<F2>"] = { "<cmd>%s/\\s\\+$//e<cr>", desc = "Remove unwanted whitespace"},
-    ["<F3>"] = { "<cmd>set spell!<cr>", desc = "Toggle spell checking"}
+    ["<F3>"] = { "<cmd>set spell!<cr>", desc = "Toggle spell checking"},
+    ["<leader>j"] = { "^", desc = "BOL" },
+    ["<leader>k"] = { "$", desc = "EOL" },
   },
   t = {
     -- setting a mapping to false will disable it
